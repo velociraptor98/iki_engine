726976bcc8676f6a8649ea5a169f55448f399d04
kunal <kunal1998doon@gmail.com>
2019-10-02 19:04:14 +0530

    Added basic sprite rendering
    Added some utility classes from Micahel Hick's toolbox that allow for some basic functions like camera and inputs. Got some sprite rendering functionality through FNA's methods.

--- iki_engine/Game1.cs
+++ iki_engine/Game1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
@@ -14,13 +15,20 @@ namespace iki_engine
 {
     class Game1:Microsoft.Xna.Framework.Game
     {
-         GraphicsDeviceManager graphics;
+         GraphicsDeviceManager graphicsManager;
          SpriteBatch spriteBatch;
-
+         private Texture2D image;
+         private Vector2 position;
+            
          public Game1()
          {
-             graphics=new GraphicsDeviceManager(this);
-             Content.RootDirectory = "Content";
+             graphicsManager=new GraphicsDeviceManager(this);
+             Content.RootDirectory = "Assets";
+             graphicsManager.PreferredBackBufferWidth = 1280;
+             graphicsManager.PreferredBackBufferHeight = 720;
+             graphicsManager.IsFullScreen = false;
+             graphicsManager.ApplyChanges();
+             position = new Vector2(640,360);
          }
 
          protected override void Initialize()
@@ -31,16 +39,37 @@ namespace iki_engine
          protected override void LoadContent()
          {
              spriteBatch = new SpriteBatch(GraphicsDevice);
+             image = TextureLoader.Load("sprite", Content);
          }
 
          protected override void Update(GameTime gameTime)
          {
-            base.Update(gameTime);
+             Input.Update();
+             if (Input.IsKeyDown(Keys.D))
+             {
+                 position.X += 1;
+             }
+             else if (Input.IsKeyDown(Keys.A))
+             {
+                 position.X -= 1;
+             }
+             else if (Input.IsKeyDown(Keys.W))
+             {
+                 position.Y -= 1;
+             }
+             else if (Input.IsKeyDown(Keys.S))
+             {
+                 position.Y += 1;
+             }
+             base.Update(gameTime);
          }
 
          protected override void Draw(GameTime gameTime)
          {
              GraphicsDevice.Clear(Color.CadetBlue);
+             spriteBatch.Begin();
+             spriteBatch.Draw(image,position,Color.White);
+             spriteBatch.End();
              base.Draw(gameTime);
          }
     }
