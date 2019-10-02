ff823ca7a0d9bae583d8a2d3b5d3858e18da3839
kunal <kunal1998doon@gmail.com>
2019-10-02 16:37:56 +0530

    setting up engine

---
+++ iki_engine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iki_engine
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
